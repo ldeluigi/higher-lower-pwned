dbc136e7b2a39cce8c7b042d3b537ae411eec9ed
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-08-23 13:07:58 +0200

    Fix default value

--- backend/js/app/utils/jwt.js
+++ backend/js/app/utils/jwt.js
@@ -5,33 +5,33 @@ const jwtValidator = require('express-jwt');
 const tokenSchema = require("../model/token").schema;
 
 module.exports = {
-    createJWT: async function (userID, username) {
-        let jwtID = pwd.genRandomString(32);
-        let jwtContent = {
-            id: userID,
-            username: username,
-            refresh: jwtID
-        };
-        let token = jwt.sign(jwtContent, config.jwtSecret, { expiresIn: "10m", algorithm: "HS256" });
-        let refresh = pwd.genRandomString(64);
-        await tokenSchema.create({
-            token: refresh,
-            expire: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7 * 8), // 8 weeks from now
-            jwtRef: jwtID,
-            user: userID
-        });
-        return {
-            token: token,
-            refresh: refresh
-        };
-    },
-    authentication: function () {
-        return jwtValidator({ secret: config.jwtSecret, requestProperty: 'auth', algorithms: ['HS256'] });
-    },
-    checkJWT: function (token, ignoreExpiration = true) {
-        return jwt.verify(token, config.jwtSecret, {
-            ignoreExpiration: ignoreExpiration,
-            algorithm: "HS256"
-        });
-    }
+  createJWT: async function (userID, username) {
+    let jwtID = pwd.genRandomString(32);
+    let jwtContent = {
+      id: userID,
+      username: username,
+      refresh: jwtID
+    };
+    let token = jwt.sign(jwtContent, config.jwtSecret, { expiresIn: "10m", algorithm: "HS256" });
+    let refresh = pwd.genRandomString(64);
+    await tokenSchema.create({
+      token: refresh,
+      expire: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7 * 8), // 8 weeks from now
+      jwtRef: jwtID,
+      user: userID
+    });
+    return {
+      token: token,
+      refresh: refresh
+    };
+  },
+  authentication: function () {
+    return jwtValidator({ secret: config.jwtSecret, requestProperty: 'auth', algorithms: ['HS256'] });
+  },
+  checkJWT: function (token, ignoreExpiration = false) {
+    return jwt.verify(token, config.jwtSecret, {
+      ignoreExpiration: ignoreExpiration,
+      algorithm: "HS256"
+    });
+  }
 }
