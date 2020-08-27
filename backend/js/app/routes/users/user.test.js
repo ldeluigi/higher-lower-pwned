501ccd8ac9e5b4eb6de0f24cfb4a6d163cdd9ca7
ldeluigi <lucadelu97@gmail.com>
2020-08-27 19:17:54 +0200

    implement user PUT test

--- backend/js/app/routes/users/user.test.js
+++ backend/js/app/routes/users/user.test.js
@@ -13,9 +13,11 @@ describe("user API", function () {
             username: "testusername",
             _id: "testid",
             password: pwd.sha512("testpassword", "testpasswordsalt"),
+            email: "testemail@email.com",
             salt: "testpasswordsalt",
+            createdAt: new Date(),
             lastLogin: new Date(),
-            save: async function (user) { }
+            save: async function () { }
         };
         mock.mockImplementation((input) => Promise.resolve(userMock));
         const mock2 = jest.spyOn(token.schema, 'create');
@@ -28,12 +30,57 @@ describe("user API", function () {
             password: "testpassword"
         });
         let result = response.body.data;
-        console.log(result)
-        response = await request.get("/users/testid").set("Authentication", "Bearer " + result.token);
+        response = await request.get("/users/testid").set("Authorization", "Bearer " + result.token);
         expect(response.status).toBe(200);
-        expect(response.body).toEqual({ data: user.toDto(userMock) });
+        expect(JSON.stringify(response.body))
+            .toEqual(JSON.stringify({ data: user.toDto(userMock) }));
         mock.mockRestore();
         mock2.mockRestore();
         done();
     });
+
+
+    it("should PUT a user after login", async (done) => {
+        const mock = jest.spyOn(user.schema, 'findOne');
+        const modifyUserBody = {
+            password: "newtestpassword",
+            email: "newtestemail@email.com"
+        };
+        const userMock = {
+            username: "testusername",
+            _id: "testid",
+            password: pwd.sha512("testpassword", "testpasswordsalt"),
+            email: "testemail@email.com",
+            salt: "testpasswordsalt",
+            createdAt: new Date(),
+            lastLogin: new Date(),
+            save: async function () { }
+        };
+
+        mock.mockImplementation((input) => Promise.resolve(userMock));
+        const mock2 = jest.spyOn(token.schema, 'create');
+        mock2.mockImplementation((input) => {
+            input._id = "createdID";
+            return Promise.resolve(input)
+        });
+        let response = await request.post("/users/login").send({
+            username: "testusername",
+            password: "testpassword"
+        });
+        console.log(response.body.errors);
+        let result = response.body.data;
+        userMock.save = async function () {
+            expect(this.password).toBe(pwd.sha512(modifyUserBody.password, this.salt));
+            expect(this.email).toBe(modifyUserBody.email);
+        };
+        response = await request.put("/users/testid").set("Authorization", "Bearer " + result.token)
+            .send(modifyUserBody);
+        expect(response.status).toBe(200);
+        expect(response.body.data).toHaveProperty("email", modifyUserBody.email);
+        expect(response.body.data).toHaveProperty("password", "updated");
+        mock.mockRestore();
+        mock2.mockRestore();
+        done();
+    });
+
 });
