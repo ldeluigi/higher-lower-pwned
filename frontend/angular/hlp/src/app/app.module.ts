dfeb2ea04bc3efbbe2bc38ec46e404e6167541c7
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-08-28 15:11:51 +0200

    creates component for global stats

--- frontend/angular/hlp/src/app/app.module.ts
+++ frontend/angular/hlp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatsComponent } from './stats/stats.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/angular/hlp/src/app/stats/stats.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Stats } from '../../model/stats';
+
+@Component({
+  selector: 'app-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.scss']
+})
+export class StatsComponent implements OnInit {
+
+  private statsUrl = 'http://localhost:8080/stats';
+  myStats = {} as Stats;
+  periods = ['day', 'week', 'month', 'year'];
+  actualPeriod = 'week';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelectPeriod(period: string): void {
+    this.getHeroes(period);
+  }
+
+  checkPeriod(p: string): boolean {
+    return p === this.actualPeriod;
+  }
+
+  getHeroes(p: string = 'week'): void {
+    this.actualPeriod = p;
+    const url = `${this.statsUrl}?period=${p}`;
+    this.http.get<Stats>(url)
+      .subscribe(stats => this.myStats = stats);
+  }
+}
