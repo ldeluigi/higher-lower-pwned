7069524498c13c78e210ab9459b701f2e9926a72
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-08-30 13:28:51 +0200

    Fix try error cases

--- backend/js/app/routes/scores/arcade.js
+++ backend/js/app/routes/scores/arcade.js
@@ -33,27 +33,31 @@ router.get(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const actualLimit = limitTools.returnLimitFromReq(req);
-    const actualPeriod = periodTools.returnPeriodFromReq(req);
-    const minMax = periodTools.periods[actualPeriod];
-    const actualPage = genericTools.getOrElse(req.query.page, 0);
-    const actualSortby = genericTools.getOrElse(req.query.sortby, valueScore);
-    const sortValue = actualSortby === valueScore ? [[valueScoreInDB, -1]] : [[valueDateInDB, 1]]
-    const result = await score
-      .find({
-        start: {
-          $gte: minMax[0],
-          $lte: minMax[1]
-        },
-        user: req.params.userid
-      })
-      .skip(actualPage * actualLimit)
-      .limit(parseInt(actualLimit))
-      .sort(sortValue);
-    if (result === null) {
-      return res.status(404).json({ errors: ["No leaderboards found"] });
+    try {
+      const actualLimit = limitTools.returnLimitFromReq(req);
+      const actualPeriod = periodTools.returnPeriodFromReq(req);
+      const minMax = periodTools.periods[actualPeriod];
+      const actualPage = genericTools.getOrElse(req.query.page, 0);
+      const actualSortby = genericTools.getOrElse(req.query.sortby, valueScore);
+      const sortValue = actualSortby === valueScore ? [[valueScoreInDB, -1]] : [[valueDateInDB, 1]]
+      const result = await score
+        .find({
+          start: {
+            $gte: minMax[0],
+            $lte: minMax[1]
+          },
+          user: req.params.userid
+        })
+        .skip(actualPage * actualLimit)
+        .limit(parseInt(actualLimit))
+        .sort(sortValue);
+      if (result === null) {
+        return res.status(404).json({ errors: ["No leaderboards found"] });
+      }
+      res.json({ data: result });
+    } catch (err) {
+      res.status(400).json({ errors: [err.message] });
     }
-    res.json({ data: result });
   }
 );
 
