444cbac9724f70243482ea1eba32c651e1921048
ldeluigi <lucadelu97@gmail.com>
2020-09-01 00:07:41 +0200

    Add socket test shell

---
+++ backend/js/app/socket/arcade.socket.test.js
@@ -0,0 +1,60 @@
+
+const app = require("../../config/server").express; // Link to your server file
+const supertest = require("supertest");
+const request = supertest(app);
+const cio = require("socket.io-client");
+const http = require("http");
+const sio = require("socket.io");
+const ioBack = require("./index");
+
+var serverListen;
+var serverAddress;
+var ioServer;
+var socket;
+
+beforeAll((done) => {
+    serverListen = http.createServer().listen();
+    serverAddress = serverListen.address();
+    ioServer = ioBack(sio(serverListen));
+    done();
+});
+
+
+afterAll((done) => {
+    ioServer.close();
+    serverListen.close();
+    done();
+});
+
+
+beforeEach((done) => {
+    // Setup
+    socket = cio.connect(
+        `http://[${serverAddress.address}]:${serverAddress.port}/arcade`,
+        {
+            "reconnection delay": 0,
+            "reopen delay": 0,
+            "force new connection": true,
+            transports: ["websocket"],
+        }
+    );
+    socket.on("connect", () => {
+        done();
+    });
+});
+
+
+afterEach((done) => {
+    // Cleanup
+    if (socket.connected) {
+        socket.disconnect();
+    }
+    done();
+});
+
+
+describe("arcade socket.io API", function () {
+    it("should be ok", async (done) => {
+        done();
+    });
+});
