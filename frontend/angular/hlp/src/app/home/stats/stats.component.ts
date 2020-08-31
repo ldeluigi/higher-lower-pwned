355d971c88bba05e55b0de55f637f8cc6fa0545f
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-08-31 12:46:53 +0200

    [frontend] fix duration visualization

--- frontend/angular/hlp/src/app/home/stats/stats.component.ts
+++ frontend/angular/hlp/src/app/home/stats/stats.component.ts
@@ -34,14 +34,34 @@ export class StatsComponent implements OnInit {
 
   private udpateStats(stats: Stats): void {
     const stat: Stat[] = [];
-    stat.push({name: 'Score', avg: stats.avgScore || 0, max: stats.maxScore || 0});
-    stat.push({name: 'Guesses', avg: stats.avgGuesses || 0, max: stats.maxGuesses || 0});
-    stat.push({name: 'PlaysPerDay', avg: stats.avgPlaysPerDay || 0, max: stats.maxPlaysPerDay || 0});
+    stat.push({ name: 'Score', avg: stats.avgScore || 0, max: stats.maxScore || 0 });
+    stat.push({ name: 'Guesses', avg: stats.avgGuesses || 0, max: stats.maxGuesses || 0 });
+    stat.push({ name: 'PlaysPerDay', avg: stats.avgPlaysPerDay || 0, max: stats.maxPlaysPerDay || 0 });
     stat.push({
       name: 'Duration (seconds)',
-      avg: stats.avgDuration || 0 / 1000,
-      max: stats.maxDuration || 0 / 1000,
+      avg: stats.avgDuration || 0,
+      max: stats.maxDuration || 0,
     });
     this.myStats = stat;
+  }
+
+  timeConversion(milliseconds: number): string {
+  // Get hours from milliseconds
+  const hours = milliseconds / (1000 * 60 * 60);
+  const absoluteHours = Math.floor(hours);
+  const h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+  // Get remainder from hours and convert to minutes
+  const minutes = (hours - absoluteHours) * 60;
+  const absoluteMinutes = Math.floor(minutes);
+  const m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
+
+  // Get remainder from minutes and convert to seconds
+  const seconds = (minutes - absoluteMinutes) * 60;
+  const absoluteSeconds = Math.floor(seconds);
+  const s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+
+  return h + ':' + m + ':' + s;
   }
 }
