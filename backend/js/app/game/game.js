81caeb7ec6af5808c5a8a17bf106530c25f05198
ldeluigi <lucadelu97@gmail.com>
2020-08-31 19:25:35 +0200

    add socket.io code (mocked by model) and update documentation according

--- backend/js/app/game/game.js
+++ backend/js/app/game/game.js
@@ -6,5 +6,28 @@ module.exports = {
     let passwordFiles = await fs.readdir(path.join(__dirname, "/passwords"));
     passwordFiles = passwordFiles.filter(fn => fn.endsWith(".csv"));
     if (passwordFiles.length <= 0) throw new Error("Password files missing.");
+  },
+  newGame: async function (gameID) {
+    return true;
+  },
+  currentGuess: async function (gameID) {
+    return {
+      password1: "p1",
+      value1: 1000,
+      password2: "p2",
+      timeout: 10000,
+      score: 100,
+      guesses: 1,
+      duration: 100000
+    };
+  },
+  deleteGame: async function (gameID) {
+    return true;
+  },
+  submitGuess: async function (gameID, guess) {
+    if (guess !== 1 && guess !== 2) {
+      throw new Error("Guess must be 1 or 2");
+    }
+    return guess === 1;
   }
 }
--- backend/js/app/socket/index.js
+++ backend/js/app/socket/index.js
@@ -11,15 +11,44 @@ module.exports = function (sio) {
       try {
         let userData = jwtTools.checkJWT(socket.handshake.query.token);
         socket.userData.id = userData.user;
-      } catch (err) { }
-      next();
+        next();
+      } catch (err) {
+        next(new Error("Authentication failed: " + err.message));
+      }
     }
     else {
       next();
     }
   }).on("connection", function (socket) {
-    console.log("Connected with id " + socket.userData);
-
+    console.log("Connected with id ", socket.userData);
+    socket.on("start", async () => {
+      let gameID = await game.newGame(socket.id);
+      let nextGuess = await game.currentGuess(socket.id);
+      socket.emit("guess", nextGuess);
+    });
+    socket.on("repeat", async () => {
+      let nextGuess = await game.currentGuess(socket.id);
+      socket.emit("guess", nextGuess);
+    });
+    socket.on("answer", async (answer) => {
+      if (answer.higher === 1 || answer.higher === 2) {
+        let isCorrect = await game.submitGuess(socket.id, answer.higher);
+        if (isCorrect) {
+          let nextGuess = await game.currentGuess(socket.id);
+          socket.emit("guess", nextGuess);
+        } else {
+          let gameInfo = await game.currentGuess(socket.id);
+          socket.emit("gameEnd", {
+            score: gameInfo.score,
+            guesses: gameInfo.guesses,
+            duration: gameInfo.duration
+          });
+        }
+      }
+    });
+    socket.on("disconnect", async (reason) => {
+      await game.deleteGame(socket.id);
+    })
   });
   return sio;
 };
