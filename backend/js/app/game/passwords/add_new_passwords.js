5c0e2a6c96392b0dce7a10bb0b3f413e4e830e7d
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-09-02 20:58:40 +0200

    Add parallelization for super fast download

--- backend/js/app/game/passwords/add_new_passwords.js
+++ backend/js/app/game/passwords/add_new_passwords.js
@@ -8,129 +8,139 @@ const csv = require('async-csv');
 
 const folder = "./";
 const debug = false;
+const readBufferLimit = 40;
 
 (async () => {
-    try {
-        await main();
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    await main();
+  } catch (e) {
+    console.error(e);
+  }
 })();
 
 async function main() {
-    try {
-        let files = await fsPromises.readdir(folder);
-        for await (const file of files) {
-            // Make one pass and make the file complete
-            var filePath = path.join(folder, file);
-            if (filePath.endsWith(".txt")) {
-                await startUpdating(filePath);
-            }
-        }
-    } catch (err) {
-        console.error("Could not list the directory.", err);
-        throw err;
+  try {
+    let files = await fsPromises.readdir(folder);
+    for await (const file of files) {
+      // Make one pass and make the file complete
+      var filePath = path.join(folder, file);
+      if (filePath.endsWith(".txt")) {
+        await startUpdating(filePath);
+      }
     }
+  } catch (err) {
+    console.error("Could not list the directory.", err);
+    throw err;
+  }
 }
 
 
 async function startUpdating(f) {
-    if (!f.endsWith(".txt")) {
-        throw new Error("Provided non txt file!");
-    }
-    let asCSV = f.replace(new RegExp("\.txt$"), ".csv");
-    let skipMap = new Map();
-    if (true == fs.existsSync(asCSV)) {
-        let csvRes = await csv.parse(await fsPromises.readFile(asCSV));
-        csvRes.shift();
-        skipMap = csvRes.reduce((map, obj) => {
-            map.set(obj[0], {
-                hash: obj[1],
-                number: obj[2]
-            });
-            return map;
-        }, new Map());
-    }
-    await fsPromises.writeFile(asCSV, "password,hash,number\n");
-    const readInterface = readline.createInterface(({
-        input: fs.createReadStream(f),
-    }));
-    for await (const line of readInterface) {
-        let trimLine = line.trim();
-        if (skipMap.has(trimLine)) {
-            let result = skipMap.get(trimLine);
-            result.password = trimLine;
-            await fsPromises.appendFile(asCSV,
-                result.password + "," +
-                result.hash + "," +
-                result.number + "\n");
-            if (debug)
-                console.log("Skipped: ", result);
-            skipMap.delete(trimLine);
-        } else {
-            let result = await downloadPasswordData(trimLine);
-            await fsPromises.appendFile(asCSV,
-                result.password + "," +
-                result.hash + "," +
-                result.number + "\n");
-            if (debug)
-                console.log(result);
+  if (!f.endsWith(".txt")) {
+    throw new Error("Provided non txt file!");
+  }
+  let asCSV = f.replace(new RegExp("\.txt$"), ".csv");
+  let skipMap = new Map();
+  if (true == fs.existsSync(asCSV)) {
+    let csvRes = await csv.parse(await fsPromises.readFile(asCSV));
+    csvRes.shift();
+    skipMap = csvRes.reduce((map, obj) => {
+      map.set(obj[0], {
+        hash: obj[1],
+        number: obj[2]
+      });
+      return map;
+    }, new Map());
+  }
+  await fsPromises.writeFile(asCSV, "password,hash,number\n");
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(f),
+  });
+  let readCount = 0;
+  let readBuffer = [];
+  for await (const line of readInterface) {
+    let trimLine = line.trim();
+    if (skipMap.has(trimLine)) {
+      let result = skipMap.get(trimLine);
+      result.password = trimLine;
+      await fsPromises.appendFile(asCSV,
+        result.password + "," +
+        result.hash + "," +
+        result.number + "\n");
+      if (debug)
+        console.log("Skipped: ", result);
+      skipMap.delete(trimLine);
+    } else {
+      readBuffer.push(trimLine);
+      readCount++;
+      if (readCount >= readBufferLimit) {
+        for await (const result of readBuffer.map(s => downloadPasswordData(s))) {
+          await fsPromises.appendFile(asCSV,
+            result.password + "," +
+            result.hash + "," +
+            result.number + "\n");
+          if (debug)
+            console.log(result);
         }
+        readBuffer = [];
+        readCount = 0;
+      }
     }
+  }
 }
 
 function downloadPasswordData(password) {
-    return new Promise((resolve, reject) => {
-        let pSHA1 = sha1(password).toUpperCase();
-        let shaBeginning = pSHA1.substr(0, 5);
+  return new Promise((resolve, reject) => {
+    let pSHA1 = sha1(password).toUpperCase();
+    let shaBeginning = pSHA1.substr(0, 5);
 
-        const options = {
-            hostname: 'api.pwnedpasswords.com',
-            port: 443,
-            path: '/range/' + shaBeginning,
-            method: 'GET'
-        };
-        const req = https.request(options, res => {
-            let str = '';
-            res.on('data', d => {
-                str += d;
-            });
-            res.on('end', () => {
-                let results = str
-                    .replace("\r", "")
-                    .split("\n")
-                    .map(x => x.split(":"))
-                    .filter(x => shaBeginning + x[0] == pSHA1)
-                    .map(x => x[1]);
-                if (results.length > 1) {
-                    throw new Error("Multiple matches on " + password);
-                } else if (results.length == 0) {
-                    resolve({
-                        password: password,
-                        hash: pSHA1,
-                        number: 0
-                    });
-                } else {
-                    resolve({
-                        password: password,
-                        hash: pSHA1,
-                        number: parseInt(results[0])
-                    });
-                }
+    const options = {
+      hostname: 'api.pwnedpasswords.com',
+      port: 443,
+      path: '/range/' + shaBeginning,
+      method: 'GET'
+    };
+    const req = https.request(options, res => {
+      let str = '';
+      res.on('data', d => {
+        str += d;
+      });
+      res.on('end', () => {
+        let results = str
+          .replace("\r", "")
+          .split("\n")
+          .map(x => x.split(":"))
+          .filter(x => shaBeginning + x[0] == pSHA1)
+          .map(x => x[1]);
+        if (results.length > 1) {
+          throw new Error("Multiple matches on " + password);
+        } else if (results.length == 0) {
+          resolve({
+            password: password,
+            hash: pSHA1,
+            number: 0
+          });
+        } else {
+          resolve({
+            password: password,
+            hash: pSHA1,
+            number: parseInt(results[0])
+          });
+        }
 
-            });
-        })
+      });
+    })
 
-        req.on('error', error => {
-            reject(error);
-        });
-        req.end();
+    req.on('error', error => {
+      reject(error);
     });
+    req.end();
+  });
 }
 
 
 function sha1(input) {
-    let shasum = crypto.createHash('sha1');
-    shasum.update(input);
-    return shasum.digest('hex');
+  let shasum = crypto.createHash('sha1');
+  shasum.update(input);
+  return shasum.digest('hex');
 }
