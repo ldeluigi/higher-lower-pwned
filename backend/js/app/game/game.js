8914b94b63641904ae17c3dacfb09dcd0b28f5d4
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-09-04 12:29:34 +0200

    Implement pick password from csv and add score based on time left

--- backend/js/app/game/game.js
+++ backend/js/app/game/game.js
@@ -1,4 +1,6 @@
-const fs = require('fs').promises;
+const fsPromises = require('fs').promises;
+const fs = require('fs');
+const readline = require('readline');
 const path = require('path');
 const gameSchema = require('../model/game').schema;
 const scoreSchema = require('../model/score').schema;
@@ -8,18 +10,29 @@ const startTimeMillis = 1000 * 10;
 const correctGuessMillis = 1000 * 5;
 const correctGuessScore = 100;
 
-async function pickPasswordAndValue() {
-  return {
-    password: "testpassword" + Math.round(Math.random() * 100),
-    value: Math.round(Math.random() * 100000)
-  };
-}
+
+const maxLinesForPasswordFile = 100000;
+const directory = __dirname + '/passwords';
+
+const fileMap = [];
 
 module.exports = {
   setup: async function () {
-    let passwordFiles = await fs.readdir(path.join(__dirname, "/passwords"));
+    let passwordFiles = await fsPromises.readdir(path.join(__dirname, "/passwords"));
     passwordFiles = passwordFiles.filter(fn => fn.endsWith(".csv"));
     if (passwordFiles.length <= 0) throw new Error("Password files missing.");
+    for (pfile of passwordFiles) {
+      let filePath = path.join(directory, pfile);
+      let lineCount = await countLines(filePath);
+      if (maxLinesForPasswordFile < lineCount) {
+        throw new Error("Password file " + pfile + "has more than " +
+          maxLinesForPasswordFile + " lines. Please split it into multiple files.");
+      }
+      fileMap.push({
+        path: filePath,
+        lines: lineCount - 1 // first line of csv is useless
+      });
+    }
   },
   newGame: async function (gameID, userID) {
     let p1 = await pickPasswordAndValue();
@@ -110,7 +123,7 @@ module.exports = {
         gameQuery.currentP2 = newP.password;
         gameQuery.valueP2 = newP.value;
         gameQuery.guesses += 1;
-        gameQuery.score += correctGuessScore;
+        gameQuery.score += correctGuessScore + Math.floor((gameQuery.expiration.getTime() - Date.now()) / 1000);
         gameQuery.expiration = new Date(gameQuery.expiration.getTime() + correctGuessMillis);
         await gameQuery.save();
       } else {
@@ -121,4 +134,46 @@ module.exports = {
     }
     return true;
   }
+}
+
+
+async function pickPasswordAndValue(wish) {
+  let max = fileMap.map(f => f.lines).reduce((a, b) => a + b, 0);
+  let pick = wish === undefined ? Math.floor(Math.random() * max) : wish;
+  let pickIndex = pick;
+  for (let i = 0; i < fileMap.length; i++) {
+    if (pick < fileMap[i].lines) {
+      let microcsv = await readOneCSVLine(fileMap[i].path, pick);
+      let split = microcsv.split(',');
+      return {
+        index: pickIndex,
+        password: split[0],
+        value: parseInt(split[2])
+      }
+    } else {
+      pick -= fileMap[i].lines;
+    }
+  }
+  throw new Error("Picked a number that exceeded maximum number of passwords available (" + max + ").");
+}
+
+async function countLines(fileName) {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(fileName),
+  });
+  let i = 0;
+  for await (_ of readInterface) i++;
+  return i;
+}
+
+async function readOneCSVLine(fileName, lineIndex) {
+  const readInterface = readline.createInterface({
+    input: fs.createReadStream(fileName),
+  });
+  let i = -1;
+  for await (const line of readInterface) {
+    if (i == lineIndex) return line;
+    i++;
+  }
+  throw new Error(fileName + " has less than " + lineIndex + " lines.");
 }
