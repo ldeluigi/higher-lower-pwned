fcf2a819d3986e6d1f4c07e76031f76106ea97e3
ldeluigi <lucadelu97@gmail.com>
2020-09-28 17:07:39 +0200

    Progress on 1v1

--- backend/js/app/socket/duel.js
+++ backend/js/app/socket/duel.js
@@ -5,6 +5,43 @@ const namespace = "/duel";
 
 const lobbyRoomPrefix = "lobby@";
 
+const matchmaking = {
+  lobbies: new Map(),
+  isPlaying: function (userID) {
+    return Array.from(this.lobbies.values()).filter(x => Array.from(x.users.keys()).includes(userID)).length > 0;
+  },
+  openRooms: function () {
+    return Array.from(this.lobbies.values()).filter(this.isOpen).map(x => x.name);
+  },
+  isOpen: function (room) {
+    return room != undefined ? room.users.size < 2 : false;
+  },
+  joinRoom: function (roomName, userID, userObj) {
+    let room = this.lobbies.get(roomName);
+    if (this.isOpen(room)) {
+      this.lobbies.get(roomName).users.set(userID, userObj);
+      return true;
+    }
+    return false;
+  },
+  createRoom: function (roomName) {
+    if (this.lobbies.has(roomName)) {
+      return false;
+    }
+    this.lobbies.set(roomName, {
+      users: new Map(),
+      name: roomName
+    });
+    return true;
+  },
+  getOpponents: function (roomName, userID) {
+    return Array.from(this.lobbies.get(roomName).users.entries()).filter(x => x[0] != userID);
+  },
+  foreachPlayerIn: function (roomName, callback) {
+    Array.from(this.lobbies.get(roomName).users.entries()).foreach(t => callback(t[0], t[1]));
+  }
+};
+
 module.exports = function (sio) {
   io = sio.of(namespace);
   io.use(function (socket, next) {
@@ -23,7 +60,7 @@ module.exports = function (sio) {
     return next();
   }).on("connection", function (socket) {
     socket.on("start", async () => {
-      if (Object.keys(socket.rooms).filter(r => r.startsWith(lobbyRoomPrefix)).length > 0) {
+      if (matchmaking.isPlaying(socket.id)) {
         socket.emit("onerror", {
           code: 105,
           description: "Already in a room."
@@ -31,39 +68,51 @@ module.exports = function (sio) {
         return;
       }
 
-      let notFullRooms = Object.entries(socket.adapter.rooms)
-        .filter(entry =>
-          entry[0].startsWith(lobbyRoomPrefix) &&
-          entry[1].length > 0 &&
-          entry[1].length < 2
-        );
+      let notFullRooms = matchmaking.openRooms();
+
       if (notFullRooms.length > 0) {
-        let myRoomName = notFullRooms[0][0];
+        let myRoomName = notFullRooms[0];
+        if (!matchmaking.joinRoom(myRoomName, socket.id, {
+          userID: socket.userData.id
+        })) {
+          socket.emit("onerror", {
+            code: 104,
+            description: "Internal server error."
+          });
+          return;
+        }
         socket.join(myRoomName);
-        let opponentID = myRoomName.slice(lobbyRoomPrefix.length);
+        let opponent = matchmaking.getOpponents(myRoomName, socket.id)[0];
         try {
-          // TODO get opponent login data
-          await duel.newGame(socket.id, opponentID, socket.userData.id);
+          await duel.newGame(socket.id, opponent[0], socket.userData.id, opponent[1].userID);
           try {
-            let nextGuess = await duel.currentGuess(socket.id);
-            socket.emit("guess", nextGuess);
-            let opponentNextGuess = await duel.currentGuess(opponentID);
-            socket.to(myRoomName).emit("guess", opponentNextGuess);
+            // TODO broadcast guess
+            // TODO delete from mm
           } catch (err) {
             socket.emit("onerror", {
-              code: 102,
+              code: 103,
               description: err.message
             });
           }
         } catch (err) {
           socket.emit("onerror", {
-            code: 101,
+            code: 102,
             description: err.message
           });
         }
       } else {
-        socket.join(lobbyRoomPrefix + socket.id);
-        socket.emit("waiting-opponents");
+        let myRoomName = lobbyRoomPrefix + socket.id;
+        if (matchmaking.createRoom(myRoomName) && matchmaking.joinRoom(myRoomName, socket.id, {
+          userID: socket.userData.id
+        })) {
+          socket.join(myRoomName);
+          socket.emit("waiting-opponents");
+        } else {
+          socket.emit("onerror", {
+            code: 101,
+            description: "Internal server error."
+          });
+        }
       }
     });
     socket.on("repeat", async () => {
@@ -74,8 +123,7 @@ module.exports = function (sio) {
         } else {
           try {
             let endData = await duel.deleteGame(socket.id, nextGuess.data[0].lost);
-            // TODO notify room
-            socket.emit("gameEnd", endData);
+            // TODO change delete game and send endData to everyone
           } catch (err) {
             socket.emit("onerror", {
               code: 202,
@@ -96,9 +144,7 @@ module.exports = function (sio) {
           let isCorrect = await duel.submitGuess(socket.id, answer.higher);
           if (isCorrect) {
             try {
-              let nextGuess = await duel.currentGuess(socket.id);
-              // TODO notify room
-              socket.emit("guess", nextGuess);
+              // TODO broadcast query
             } catch (err) {
               socket.emit("onerror", {
                 code: 302,
@@ -108,7 +154,7 @@ module.exports = function (sio) {
           } else {
             try {
               let endData = await duel.deleteGame(socket.id);
-              socket.emit("gameEnd", endData);
+              // TODO send endData to everyone
             } catch (err) {
               socket.emit("onerror", {
                 code: 303,
@@ -127,7 +173,7 @@ module.exports = function (sio) {
     socket.on("disconnect", async (reason) => {
       try {
         await duel.deleteGame(socket.id);
-        // TODO notify room
+        // TODO notify everyone
         console.log(socket.rooms)
       } catch (err) { }
     })
