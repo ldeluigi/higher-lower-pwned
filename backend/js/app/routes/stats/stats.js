254889750ec28af4f42ec73847e47cfe46d7e544
Simone <Simone.magnani4@studio.unibo.it>
2020-10-12 17:44:21 +0200

    [backend] hint added to fix avgplaysforday

--- backend/js/app/routes/stats/stats.js
+++ backend/js/app/routes/stats/stats.js
@@ -4,95 +4,95 @@ const { validationResult, query } = require("express-validator");
 const periodTools = require("../../helpers/period");
 const score = require("../../model/score").schema;
 
-router.get("/",
-  [
-    periodTools.checkPeriod(query("period"))
-  ],
+router.get(
+  "/",
+  [periodTools.checkPeriod(query("period"))],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     const queryPeriod = req.query.period || periodTools.default;
-    const statsBeginDate = periodTools.subtractPeriodNTimesFromToday(queryPeriod, 1);
+    const statsBeginDate = periodTools.subtractPeriodNTimesFromToday(
+      queryPeriod,
+      1
+    );
     const totalDays = periodTools.periodsInDays(queryPeriod);
 
     try {
-      let result = await score
-        .aggregate([
-          {
-            $match: {
-              end: {
-                $gte: statsBeginDate,
-                $lte: new Date()
-              },
-              mode: "arcade"
-            }
+      let result = await score.aggregate([
+        {
+          $match: {
+            end: {
+              $gte: statsBeginDate,
+              $lte: new Date(),
+            },
+            mode: "arcade",
           },
-          {
-            $project: {
-              day: { $dayOfYear: "$end" },
-              week: { $week: "$end" },
-              month: { $month: "$end" },
-              year: { $year: "$end" },
-              score: true,
-              guesses: true,
-              start: true,
-              end: true,
-
-            }
+        },
+        {
+          $project: {
+            day: { $dayOfYear: "$end" },
+            week: { $week: "$end" },
+            month: { $month: "$end" },
+            year: { $year: "$end" },
+            score: true,
+            guesses: true,
+            start: true,
+            end: true,
           },
-          {
-            $facet: {
-              allTime: [
-                {
-                  $group: {
-                    _id: null,
-                    maxScore: { $max: "$score" },
-                    avgScore: { $avg: "$score" },
-                    maxGuesses: { $max: "$guesses" },
-                    avgGuesses: { $avg: "$guesses" },
-                    maxDuration: { $max: { $subtract: ["$end", "$start"] } },
-                    avgDuration: {
-                      $avg: { $subtract: ["$end", "$start"] }
-                    },
-                  }
-                }
-              ],
-              byDay: [
-                {
-                  $group: {
-                    _id: {
-                      day: "$day"
-                    },
-                    plays: { $sum: 1 }
-                  }
+        },
+        {
+          $facet: {
+            allTime: [
+              {
+                $group: {
+                  _id: null,
+                  maxScore: { $max: "$score" },
+                  avgScore: { $avg: "$score" },
+                  maxGuesses: { $max: "$guesses" },
+                  avgGuesses: { $avg: "$guesses" },
+                  maxDuration: { $max: { $subtract: ["$end", "$start"] } },
+                  avgDuration: {
+                    $avg: { $subtract: ["$end", "$start"] },
+                  },
                 },
-                {
-                  $group: {
-                    _id: null,
-                    avgPlaysPerDay: { $avg: "$plays" },
-                    maxPlaysPerDay: { $max: "$plays" }
-                  }
-                }
-              ]
-            }
+              },
+            ],
+            byDay: [
+              {
+                $group: {
+                  _id: {
+                    day: "$day",
+                  },
+                  plays: { $sum: 1 },
+                },
+              },
+              {
+                $group: {
+                  _id: null,
+                  avgPlaysPerDay: { $avg: "$plays" },
+                  maxPlaysPerDay: { $max: "$plays" },
+                },
+              },
+            ],
           },
-          {
-            $project: {
-              avgScore: { $first: "$allTime.avgScore" },
-              maxScore: { $first: "$allTime.maxScore" },
-              avgGuesses: { $first: "$allTime.avgGuesses" },
-              maxGuesses: { $first: "$allTime.maxGuesses" },
-              avgDuration: { $first: "$allTime.avgDuration" },
-              maxDuration: { $first: "$allTime.maxDuration" },
-              avgPlaysPerDay: { $first: "$byDay.avgPlaysPerDay" },
-              maxPlaysPerDay: { $first: "$byDay.maxPlaysPerDay" }
-            }
-          }
-        ]);
+        },
+        {
+          $project: {
+            avgScore: { $first: "$allTime.avgScore" },
+            maxScore: { $first: "$allTime.maxScore" },
+            avgGuesses: { $first: "$allTime.avgGuesses" },
+            maxGuesses: { $first: "$allTime.maxGuesses" },
+            avgDuration: { $first: "$allTime.avgDuration" },
+            maxDuration: { $first: "$allTime.maxDuration" },
+            avgPlaysPerDay: { $first: "$byDay.avgPlaysPerDay" }, //{ $first: $byDay.maxPlaysPerDay / totalDays }
+            maxPlaysPerDay: { $first: "$byDay.maxPlaysPerDay" },
+          },
+        },
+      ]);
       res.json({
-        data: result[0]
+        data: result[0],
       });
     } catch (err) {
       res.status(400).json({ errors: [err.message] });
