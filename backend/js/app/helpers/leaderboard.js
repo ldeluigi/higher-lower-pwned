263689b894a640459edcdc4268faf1899d53e0d8
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-10-15 10:55:59 +0200

    Remove leaderboard code repetitions

---
+++ backend/js/app/helpers/leaderboard.js
@@ -0,0 +1,49 @@
+const { validationResult, query } = require("express-validator");
+const score = require("../model/score").schema;
+const scoreToDto = require("../model/score").toDto;
+const periodTools = require("./period");
+const limitTools = require("./limit");
+
+module.exports = {
+  guards: [
+    periodTools.checkPeriod(query("period")),
+    limitTools.checkLimit(query("limit"))
+  ],
+  requestHandler: function (modeInDatabase) {
+    return (async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const queryLimit = req.query.limit || limitTools.limit;
+      const queryPeriod = req.query.period || periodTools.default;
+      const minMax = periodTools.periods[queryPeriod];
+      const result = await score
+        .find({
+          end: {
+            $gte: minMax[0],
+            $lte: minMax[1],
+          },
+          mode: modeInDatabase
+        })
+        .limit(parseInt(queryLimit))
+        .sort({
+          score: "desc",
+          end: "desc"
+        })
+        .populate('user');
+      if (result === null) {
+        return res.status(400).json({ errors: ["Score query error."] });
+      }
+      let mappedScores = result.map(s => {
+        if (s.user !== undefined && s.user !== null) {
+          return scoreToDto(s, s.user.username);
+        }
+        return scoreToDto(s);
+      });
+      res.json({
+        data: mappedScores,
+      });
+    });
+  }
+}
--- backend/js/app/routes/leaderboards/duel.js
+++ backend/js/app/routes/leaderboards/duel.js
@@ -1,52 +1,11 @@
 const express = require("express");
-const { validationResult, query } = require("express-validator");
-const score = require("../../model/score").schema;
-const scoreToDto = require("../../model/score").toDto;
 const router = express.Router();
-const periodTools = require("../../helpers/period");
-const limitTools = require("../../helpers/limit");
+const leaderboardHelper = require("../../helpers/leaderboard");
 
 router.get(
-    "/duel",
-    [
-        periodTools.checkPeriod(query("period")),
-        limitTools.checkLimit(query("limit"))
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const queryLimit = req.query.limit || limitTools.limit;
-        const queryPeriod = req.query.period || periodTools.default;
-        const minMax = periodTools.periods[queryPeriod];
-        const result = await score
-            .find({
-                end: {
-                    $gte: minMax[0],
-                    $lte: minMax[1],
-                },
-                mode: /^duel\.(win|lost)$/
-            })
-            .limit(parseInt(queryLimit))
-            .sort({
-                score: "desc",
-                end: "desc"
-            })
-            .populate('user');
-        if (result === null) {
-            return res.status(400).json({ errors: ["Score query error."] });
-        }
-        let mappedScores = result.map(s => {
-            if (s.user !== undefined && s.user !== null) {
-                return scoreToDto(s, s.user.username);
-            }
-            return scoreToDto(s);
-        });
-        res.json({
-            data: mappedScores,
-        });
-    }
+  "/duel",
+  leaderboardHelper.guards,
+  leaderboardHelper.requestHandler(/^duel\.(win|lost)$/)
 );
 
 module.exports = router;
