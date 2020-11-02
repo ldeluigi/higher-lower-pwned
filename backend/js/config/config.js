e3877df84d071b49c3ba2df6e211dc47bac000f8
ldeluigi <lucadelu97@gmail.com>
2020-11-02 16:30:43 +0100

    Update mailer config

--- backend/js/config/config.js
+++ backend/js/config/config.js
@@ -1,17 +1,21 @@
 
 function emailTransport() {
-  if (process.env.GMAIL_EMAIL && process.env.GMAIL_PASSWORD) {
-    return {
-      service: 'gmail',
-      auth: {
-        user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_PASSWORD
-      }
-    };
+  if (process.env.MAILER_EMAIL && process.env.MAILER_PASSWORD) {
+    if (process.env.MAILER_EMAIL.includes("@gmail.")) {
+      return {
+        service: 'gmail',
+        auth: {
+          user: process.env.MAILER_EMAIL,
+          pass: process.env.MAILER_PASSWORD
+        }
+      };
+    } else {
+      throw new Error("Email service not yet supported. Visit https://nodemailer.com/smtp/well-known/.");
+    }
   } else if (process.env.EMAIL_DEBUG === "true" || process.env.NODE_ENV === "test") {
     return null;
   } else {
-    console.log(process.env.EMAIL_DEBUG)
+    console.log("EMAIL_DEBUG: " + process.env.EMAIL_DEBUG)
     throw new Error("Service not configured error. Write your own code to manage your email account with nodemailer");
   }
 }
