4d24205b05e7706ca67082aa58d23c6987e1df28
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-11-21 19:03:58 +0100

    [frontend] test fix

--- frontend/angular/hlp/src/app/routes/game/components/counter/counter.component.spec.ts
+++ frontend/angular/hlp/src/app/routes/game/components/counter/counter.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CounterComponent } from './counter.component';
 
@@ -16,7 +17,8 @@ describe('CounterComponent', () => {
         RouterTestingModule,
         HttpClientTestingModule,
         MatSnackBarModule,
-        MatDialogModule
+        MatDialogModule,
+        BrowserAnimationsModule
       ],
       declarations: [ CounterComponent ]
     })
