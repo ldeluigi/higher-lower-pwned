337834a9d63cfacde1b8cd2bb47fcd06c4d729d7
Simone <Simone.magnani4@studio.unibo.it>
2020-11-21 10:10:05 +0100

    [frontend] working on user-scores

--- frontend/angular/hlp/src/app/routes/users/user-scores/user-scores.component.ts
+++ frontend/angular/hlp/src/app/routes/users/user-scores/user-scores.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserScoresService } from "../../../services/user-scores.service";
 import { AccountService } from '../../../services/account.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { RequestScore } from 'src/app/model/users/scores/requestScore';
 import { MatTableDataSource } from '@angular/material/table';
 import { UserScores, CoreUserScores } from 'src/app/model/users/scores/modeScore';
@@ -31,12 +31,14 @@ export class UserScoresComponent implements OnInit, OnDestroy {
 
   displayedColumns = this.defaultColumns;
   dataSource = new MatTableDataSource<CoreUserScores>([]);
+  selected = "arcade";
   
 
   constructor(
     private router: Router,
     private accountService: AccountService,
-    private scoreService:UserScoresService) { }
+    private scoreService:UserScoresService
+  ) { }
 
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
@@ -46,12 +48,42 @@ export class UserScoresComponent implements OnInit, OnDestroy {
     if (this.accountService.userValue === null) {
       this.router.navigate(['/login']);
     } else {
-      this.sub = this.scoreService.getArcadeScores(this.haveToBeDeleted).subscribe((data) => {
-        this.dataSource.data = data.data; 
-      });
-      this.sub = this.scoreService.getDuelScores(this.haveToBeDeleted).subscribe((data) => {
-        this.dataSource.data = data.data; 
-      });
+      this.select(this.selected);
     }
+  }
+
+  select(mode: string) {
+    const tmp = this.selected;
+    this.selected = mode;
+    switch (mode) {
+      case 'arcade':
+        this.updateData(this.scoreService.getArcadeScores(this.haveToBeDeleted));
+        break;
+      case 'duel':
+        this.updateData(this.scoreService.getDuelScores(this.haveToBeDeleted));
+        break;
+      case 'duelWin':
+        this.updateData(this.scoreService.getDuelScores(this.haveToBeDeleted));
+        break;
+      case 'royale':
+        this.updateData(this.scoreService.getRoyaleScores(this.haveToBeDeleted));
+        break;
+      case 'royaleWin':
+        this.updateData(this.scoreService.getRoyaleScores(this.haveToBeDeleted));
+        break;
+      default:
+        this.selected = tmp;
+        throw new Error("unknown mode");
+    }
+  }
+
+  isSelected(mode: string) {
+    return mode === this.selected;
+  }
+
+  private updateData( obs: Observable<UserScores>) {
+    this.sub = obs.subscribe((data) => {
+      this.dataSource.data = data.data; 
+    });
   }
 }
