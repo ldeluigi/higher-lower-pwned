eea31a87eb96f717ca6f05abdccaca554d23e106
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2020-11-24 16:35:17 +0100

    Add support for userID checks

--- backend/js/app/utils/matchmaking.js
+++ backend/js/app/utils/matchmaking.js
@@ -10,8 +10,15 @@ class MatchMaking {
   constructor() {
     this.room = new Room();
   }
-  isInRoom(userID) {
-    return Array.from(this.room.users.keys()).includes(userID);
+  isInRoom(userID, objUserID) {
+    console.log(objUserID, Array.from(this.room.users.values())
+      .map(x => x.userID))
+    return Array.from(this.room.users.keys()).includes(userID) ||
+      (objUserID &&
+        Array.from(this.room.users.values())
+          .map(x => x.userID)
+          .includes(objUserID)
+      );
   }
   isOpen() {
     return this.room.open;
@@ -34,7 +41,7 @@ class MatchMaking {
     return Array.from(this.room.users.entries()).filter(x => x[0] != userID);
   }
   /**
-   * @param {(id: String, obj) => void} callback 
+   * @param {(id: String, obj) => void} callback
    */
   foreachPlayerIn(callback) {
     Array.from(this.room.users.entries()).forEach(t => callback(t[0], t[1]));
