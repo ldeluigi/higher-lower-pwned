c80999278e33f3be0b9cf4cd5472a2c88074299c
Simone <Simone.magnani4@studio.unibo.it>
2020-11-24 13:30:58 +0100

    [frontend] fix test bug

--- frontend/angular/hlp/src/app/routes/stats/leaderboard/leadeboard.component.spec.ts
+++ frontend/angular/hlp/src/app/routes/stats/leaderboard/leadeboard.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LeadeboardComponent } from './leadeboard.component';
 import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 
 describe('LeadeboardComponent', () => {
@@ -12,6 +13,7 @@ describe('LeadeboardComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        NoopAnimationsModule,
         HttpClientTestingModule,
         RouterTestingModule,
         MatSelectModule,
