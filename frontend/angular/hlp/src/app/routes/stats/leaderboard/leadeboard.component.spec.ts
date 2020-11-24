861a1decc165e53d89bb60f779c708edfb372cf3
Simone <Simone.magnani4@studio.unibo.it>
2020-11-24 12:35:57 +0100

    [frontend] add mat-select to leaderboard

--- frontend/angular/hlp/src/app/routes/stats/leaderboard/leadeboard.component.spec.ts
+++ frontend/angular/hlp/src/app/routes/stats/leaderboard/leadeboard.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LeadeboardComponent } from './leadeboard.component';
+import { MatSelectModule } from '@angular/material/select';
 
 
 describe('LeadeboardComponent', () => {
@@ -13,6 +14,7 @@ describe('LeadeboardComponent', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
+        MatSelectModule,
       ],
       declarations: [ LeadeboardComponent ]
     })
