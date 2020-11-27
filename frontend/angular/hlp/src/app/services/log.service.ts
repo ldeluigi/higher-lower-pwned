83218d2eb28977b78116e3d3bbadc592181c1719
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-11-27 17:52:31 +0100

    [frontend] create log service

---
+++ frontend/angular/hlp/src/app/services/log.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from 'src/environments/environment';
+import { LogLevel } from '../model/logLevel';
+import { OnError } from '../routes/game/model/error';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+
+  readonly maxLogInProduction = LogLevel.All;
+  private isProduction = false;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    ) {
+    if (environment.production) {
+      this.isProduction = true;
+    }
+  }
+
+  log(message: string, severity: LogLevel, withDate: boolean = false): void {
+    if (this.isProduction && severity > this.maxLogInProduction) {
+      // skip this log in production
+      return;
+    }
+    let value = '';
+    if (withDate) {
+      value = `${new Date()} - `;
+    }
+    value = value.concat(`[${severity.toString()}]: ${message}`);
+  }
+
+  messageSnackBar(message: string, duration: number = 3000): void {
+    this.snackBar.open(message, undefined, { duration });
+  }
+
+  infoSnackBar(message: string, duration: number = 10000): void {
+    this.snackBar.open(message, 'OK', { duration });
+  }
+
+  errorSnackBar(error: OnError | string, duration: number = 3000): void {
+    const oe = error as OnError;
+    let value = '';
+    if (oe.code) {
+      value = `[${oe.code}] ${oe.description}`;
+    } else {
+      value = error.toString();
+    }
+    this.snackBar.open(value, undefined, { duration });
+  }
+}
