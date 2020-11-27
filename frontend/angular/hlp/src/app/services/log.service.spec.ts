beea6642ec220891b42b8b694436c6d9dba4cff3
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-11-27 17:54:22 +0100

    [frontend] test fix

--- frontend/angular/hlp/src/app/services/log.service.spec.ts
+++ frontend/angular/hlp/src/app/services/log.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { LogService } from './log.service';
 
@@ -6,7 +7,11 @@ describe('LogService', () => {
   let service: LogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        MatSnackBarModule
+      ]
+    });
     service = TestBed.inject(LogService);
   });
 
