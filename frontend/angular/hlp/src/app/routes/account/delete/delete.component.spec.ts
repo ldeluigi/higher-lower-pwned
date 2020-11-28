86d7bdcb1a3679782f46b8530c2f7132032c87ff
Simone <Simone.magnani4@studio.unibo.it>
2020-11-28 10:48:58 +0100

    [frontend] update delete component test

--- frontend/angular/hlp/src/app/routes/account/delete/delete.component.spec.ts
+++ frontend/angular/hlp/src/app/routes/account/delete/delete.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { DeleteComponent } from './delete.component';
 
 describe('DeleteComponent', () => {
@@ -8,6 +11,18 @@ describe('DeleteComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ 
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        MatDialogModule,
+      ],      
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+      ],
       declarations: [ DeleteComponent ]
     })
     .compileComponents();
