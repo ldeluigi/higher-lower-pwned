06391b4d505e012ef65b39a9cfc5e9ae8181ea39
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-11-30 18:28:02 +0100

    [frontend] add hammerjs and gesture for mobile sidebar

--- frontend/angular/hlp/src/app/app.module.ts
+++ frontend/angular/hlp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HammerModule, HAMMER_GESTURE_CONFIG} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +10,19 @@ import { LayoutComponent } from './layout/layout.component';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 import { HomeModule } from './routes/home/home.module';
 import { SharedModule } from './shared/shared.module';
+
+import * as Hammer from 'hammerjs';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = {
+    swipe: {
+      threshold:	20,
+      direction: Hammer.DIRECTION_HORIZONTAL
+    },
+    pinch: { enable: false },
+    rotate: { enable: false }
+  };
+}
 
 @NgModule({
   declarations: [
@@ -24,10 +37,15 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     BrowserAnimationsModule,
     HomeModule,
+    HammerModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
   ],
   bootstrap: [AppComponent]
 })
