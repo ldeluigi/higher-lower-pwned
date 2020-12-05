62fa1a9e39fdbf745d11db059150f4eba1105361
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2020-12-05 16:55:44 +0100

    [frontend] remove hammerjs

--- frontend/angular/hlp/src/main.ts
+++ frontend/angular/hlp/src/main.ts
@@ -1,11 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-/**
- * Hammerjs must be imported for gestures
- */
-import 'hammerjs';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
