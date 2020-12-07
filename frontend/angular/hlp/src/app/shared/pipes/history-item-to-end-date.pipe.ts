67be24c3b6c9db3e4474c62ca09e58c323fd9624
Simone <Simone.magnani4@studio.unibo.it>
2020-12-07 17:26:26 +0100

    [frontend] fix a consoled error

--- frontend/angular/hlp/src/app/shared/pipes/history-item-to-end-date.pipe.ts
+++ frontend/angular/hlp/src/app/shared/pipes/history-item-to-end-date.pipe.ts
@@ -12,19 +12,20 @@ export class HistoryItemToEndDatePipe implements PipeTransform {
       period = 'week';
     }
     let date: Date = new Date();
-    if (period === 'day') {
-      date = convertDayNumberInDate(value.periodNumber, value.year);
-    } else if (period === 'week') {
-      const weekVariance = new Date(date.getFullYear(), 0, 1).getDay();
-      date = new Date(value.year, 0, 7 * (value.periodNumber + 1) - weekVariance);
-    } else if (period === 'month') {
-      date = new Date(value.year, value.periodNumber, 0);
-    } else if (period === 'year') {
-      date = new Date(value.year + 1, 0, 0);
-    } else {
-      throw new Error('Invalid period');
+    if (value.periodNumber !== undefined) {
+      if (period === 'day') {
+        date = convertDayNumberInDate(value.periodNumber, value.year);
+      } else if (period === 'week') {
+        const weekVariance = new Date(date.getFullYear(), 0, 1).getDay();
+        date = new Date(value.year, 0, 7 * (value.periodNumber + 1) - weekVariance);
+      } else if (period === 'month') {
+        date = new Date(value.year, value.periodNumber, 0);
+      } else if (period === 'year') {
+        date = new Date(value.year + 1, 0, 0);
+      } else {
+        throw new Error('Invalid period');
+      }
     }
-
     return date;
   }
 
