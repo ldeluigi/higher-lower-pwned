0556180094e9fa702120f4c3da8284b98acc8fed
Luca Deluigi <lucadelu97@gmail.com>
2021-02-23 12:02:39 +0100

    Disable sharding

--- backend/mongo/setup/init-mongo.sh
+++ backend/mongo/setup/init-mongo.sh
@@ -10,13 +10,13 @@ mongo -- "$MONGO_INITDB_DATABASE" <<-EOJS
     db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOJS
 
-{
-sleep 3 &&
-mongo -- "$MONGO_INITDB_DATABASE" <<-EOJS
-    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
-    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
-    var admin = db.getSiblingDB('admin');
-    admin.auth(rootUser, rootPassword);
-    rs.initiate()
-EOJS
-} &
+# {
+# sleep 3 &&
+# mongo -- "$MONGO_INITDB_DATABASE" <<-EOJS
+#     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+#     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+#     var admin = db.getSiblingDB('admin');
+#     admin.auth(rootUser, rootPassword);
+#     rs.initiate()
+# EOJS
+# } &
